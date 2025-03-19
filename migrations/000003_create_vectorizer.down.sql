e4c69f35db38d65424ea8492692031a8da947d1b
Emad Mokhtar <me@emadmokhtar.com>
2025-03-19 12:26:50 +0100

    Upgrade the chunking and fix the vectorizor migration

--- migrations/000003_create_vectorizer.down.sql
+++ migrations/000003_create_vectorizer.down.sql
@@ -1,1 +1,11 @@
-SELECT ai.drop_vectorizer((SELECT v.id FROM ai.vectorizer v where v.source_table = 'yt_videos' LIMIT 1));
+DO $$
+DECLARE
+    embedding_table varchar;
+    vectorizer_id integer;
+BEGIN
+    SELECT v.target_table INTO embedding_table FROM ai.vectorizer v WHERE v.source_table = 'yt_videos' LIMIT 1;
+    SELECT v.id INTO vectorizer_id FROM ai.vectorizer v WHERE v.source_table = 'yt_videos' LIMIT 1;
+
+    PERFORM ai.drop_vectorizer(vectorizer_id);
+    EXECUTE 'DROP TABLE IF EXISTS ' || embedding_table || ' CASCADE';
+END $$;
