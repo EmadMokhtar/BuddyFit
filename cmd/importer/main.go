dbec99013aa9124609d868a7fea8d524f7a53b2f
Emad Mokhtar <me@emadmokhtar.com>
2025-10-11 14:56:55 +0200

    feat(error-handling): Improve error handling in database connection and template execution

--- cmd/importer/main.go
+++ cmd/importer/main.go
@@ -17,10 +17,18 @@ func main() {
 	ctx := context.Background()
 	conn, err := pgx.Connect(ctx, dsn)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		_, err := fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		if err != nil {
+			return
+		}
 		os.Exit(1)
 	}
-	defer conn.Close(ctx)
+	defer func(conn *pgx.Conn, ctx context.Context) {
+		err = conn.Close(ctx)
+		if err != nil {
+
+		}
+	}(conn, ctx)
 
 	// Read all the files in the data directory and insert them into the database
 	err = filepath.Walk(dataDir, func(path string, info os.FileInfo, err error) error {
@@ -35,12 +43,18 @@ func main() {
 			// Result should be: Video ID: 0FE_0zjjaQs, Title: The Truth About Genetics and YOUR Muscle Growth, Author: file directory name
 			video, err := internal.NewVideoFromFileName(info.Name(), path)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "Unable to create video from file name: %v\n", err)
+				_, err = fmt.Fprintf(os.Stderr, "Unable to create video from file name: %v\n", err)
+				if err != nil {
+					return err
+				}
 				return nil // Continue processing other files
 			}
 			_, err = conn.Exec(ctx, "INSERT INTO yt_videos (author, title, transcript, url) VALUES ($1, $2, $3, $4)", video.Author, video.Title, video.Transcript, video.UnescapedURL())
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "Unable to insert data: %v\n", err)
+				_, err = fmt.Fprintf(os.Stderr, "Unable to insert data: %v\n", err)
+				if err != nil {
+					return err
+				}
 				os.Exit(1)
 			}
 			fmt.Println(path)
